31d7f19ce98796269930a9b4d4e9a65657e5300d
Daria1386 <141306626+Daria1386@users.noreply.github.com>
2023-08-16 16:12:05 +0300

    Delete qCoda-d directory

--- qCoda-d/line/switchToLine.spec.js
+++
@@ -1,8 +0,0 @@
-it('переключение на Линии', async function () {
-    await sleep(2000);
-    const oldBtn = await driver.findElement(By.className('active')).getText();
-    const btnLine = await driver.findElement(By.xpath('//*[@id="tabs"]/li[2]/a')).click();
-    await sleep(2000);
-    const newBtn = await driver.findElement(By.className('active')).getText();
-    assert.notEqual(oldBtn, newBtn, 'Переключение не произошло');
-});
